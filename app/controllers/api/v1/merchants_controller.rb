eecdb01bc66e7280a27902250dce104866a840c1
Whitney Kidd <whitneymkidd@gmail.com>
2020-07-11 11:24:55 -0600

    create and pass merchant create_spec

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -6,4 +6,15 @@ class Api::V1::MerchantsController < ApplicationController
   def show
     render json: MerchantSerializer.new(Merchant.find(params[:id]))
   end
+
+  def create
+    merchant = Merchant.new(merchant_params)
+    render json: MerchantSerializer.new(merchant) if merchant.save
+  end
+
+  private
+
+  def merchant_params
+    params.permit(:name)
+  end
 end
---
+++ spec/requests/api/v1/merchants/create_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe 'Merchant Create' do
+  it "can create a new merchant" do
+    merchant = build(:merchant)
+
+    post "/api/v1/merchants", params: { name: merchant.name}
+
+    json = JSON.parse(response.body, symbolize_names: true)
+    last_merchant = Merchant.last
+
+    expect(response).to be_successful
+    expect(json[:data][:type]).to eq('merchant')
+    expect(json[:data][:attributes][:name]).to eq(merchant.name)
+
+    expect(last_merchant.name).to eq(merchant.name)
+  end
+end
