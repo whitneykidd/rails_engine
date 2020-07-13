b8b42ecab6e97486d3f515a5f3561f274db2d650
Whitney Kidd <whitneymkidd@gmail.com>
2020-07-13 17:06:50 -0600

    create items/find and items/find_all routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@ Rails.application.routes.draw do
       resources :merchants, except: [:new, :edit] do
         resources :items, only: [:index]
       end
+      namespace :items do
+        get :find, to: "searches#show"
+        get :find_all, to: "searches#index"
+      end
       resources :items, except: [:new, :edit] do
         get 'merchant', to: "merchants#show"
       end
