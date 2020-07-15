ddd51b333000b1afeba4fdd0b8f015e444c96a1f
Whitney Kidd <whitneymkidd@gmail.com>
2020-07-15 13:24:33 -0600

    pass merchant with most items and merchant with most revenue

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -5,4 +5,23 @@ class Merchant < ApplicationRecord
   has_many :transactions, through: :invoices
 
   validates :name, presence: true
+
+  def self.most_revenue(limit = 10)
+    select("merchants.*, \
+      SUM(invoice_items.quantity * invoice_items.unit_price) AS revenue")
+      .joins(invoices: [:invoice_items, :transactions])
+      .merge(Transaction.where(result: 'success'))
+      .group(:id)
+      .order('revenue DESC')
+      .limit(limit)
+  end
+
+  def self.most_items_sold(limit = 10)
+    select('merchants.*, SUM(invoice_items.quantity) AS items_sold')
+      .joins(invoices: [:invoice_items, :transactions])
+      .merge(Transaction.where(result: 'success'))
+      .group(:id)
+      .order('items_sold DESC')
+      .limit(limit)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
       namespace :merchants do
         get :find, to: "searches#show"
         get :find_all, to: "searches#index"
+        get :most_revenue, to: "revenues#index"
+        get :most_items, to: "items#index"
       end
       resources :merchants, except: [:new, :edit] do
         resources :items, only: [:index]
